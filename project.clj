0724ca38a35ab0f33781e4794573188c7d165a52
Mochamad Lucky Pradana <luckypradana123@yandex.com>
2018-11-25 16:37:03 +0700

    test compile less to css

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [environ "1.1.0"]
+                 [hawk "0.2.11"]]
+  :profiles {:dev {:env {:less-path ["resources/less"]}}})
--- src/less_file_watcher/core.clj
+++ src/less_file_watcher/core.clj
@@ -1,6 +1,40 @@
-(ns less-file-watcher.core)
+(ns less-file-watcher.core
+  (:use [clojure.java.shell :only [sh]])
+  (:require [hawk.core :as hawk]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn compile-less [src-file map? min?]
+  (let [file-path (.getPath src-file)
+        file-name-with-extension (.getName src-file)
+        file-name (let [vec-s (clojure.string/split file-name-with-extension #"\.")
+                        count-vec-s (count vec-s)]
+                    (clojure.string/join (subvec vec-s 0 (dec count-vec-s))))
+        file-parent-dir (.getParent src-file)
+        map (if map? "--source-map" "")
+        min (if min? "--clean-css" "")
+        opt [min map]
+        dst (str file-parent-dir "/" file-name ".css" )
+        ;cmd (clojure.string/join " " ["lessc" file-path dst (clojure.string/join " " opt)])
+        ]
+    (println (str "Compiling " file-name-with-extension))
+    (sh "lessc" file-path dst map min)
+    (println (str "Compiled.")))
+  )
+
+(defn -main [& args]
+  (hawk/watch! [{:paths ["resources/less"]
+                 :filter hawk/file?
+                 :handler (fn [ctx {:keys [file kind]}]
+                            (let [vec-s (clojure.string/split (.getName file) #"\.")
+                                  count-vec-s (count vec-s)
+                                  extension (first (subvec vec-s (dec count-vec-s)))
+                                  file-name-without-extension (clojure.string/join (subvec vec-s 0 (dec count-vec-s)))]
+                              (if (= "less" extension)
+                                ;; if it's a LESS file
+                                (do
+                                  (compile-less file true true)
+                                  (println ctx)
+                                  (assoc ctx (keyword file-name-without-extension) []))
+                                ;; else
+                                (do
+                                  (println ctx)
+                                  ctx))))}]))
